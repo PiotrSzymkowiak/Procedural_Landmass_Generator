670e30e7c2800e0d33ef414e2ed4157c295db5ec
Piotr <p.szymkowiak@capybarasolutions.pl>
2018-10-23 17:23:54 +0200

    init commit (lesson/episode 02)

---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise {
+
+	public static float[,] GenerateNoiseMap(int mapHeight, int mapWidth, float scale)
+	{
+		float[,] noiseMap = new float[mapHeight, mapWidth];
+
+		if(scale <= 0)
+		{
+			scale = 0.0001f;
+		}
+
+		for(int y = 0; y < mapHeight; y++)
+		{
+			for(int x = 0; x < mapWidth; x++)
+			{
+				float sampleX = x / scale;
+				float sampleY = y / scale;
+
+				float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+				noiseMap[x,y] = perlinValue;
+			}
+		}
+
+		return noiseMap;
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+This repository is for learning purposes and corresponds to Sebastian's Lague tutorial about this topic:
+https://www.youtube.com/playlist?list=PLFt_AvWsXl0eBW2EiBtl_sxmDtSgZBxB3
